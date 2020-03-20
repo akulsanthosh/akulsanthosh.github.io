6dd86efae298b1c5cfcff2bff9d6eb7859145c6f
Akul Santhosh <akul753@gmail.com>
2020-03-20 14:25:20 +0530

    Added Convai

---
+++ convai/UIBuilder/data.js
@@ -0,0 +1,50 @@
+let data = {
+    "content": {
+        "item1": {
+            "type": "ImageCard",
+            "image": "<img src='https://zippypaws.com/app/uploads/2018/05/strawberry-waffles-1024x668.jpg'>",
+            "title": "TITLE",
+            "subtitle": "SUBTITLE",
+            "no": "$34",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam semper vulputate dolor quis vehicula. Proin lobortis nisi sed massa tempus feugiat. Sed a justo ligula. Integer"
+        },
+        "item2": {
+            "type": "ImageCard",
+            "image": "<img src='https://zippypaws.com/app/uploads/2018/05/strawberry-waffles-1024x668.jpg'>",
+            "title": "TITLE",
+            "subtitle": "SUBTITLE",
+            "no": "$34",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam semper vulputate dolor quis vehicula. Proin lobortis nisi sed massa tempus feugiat. Sed a justo ligula. Integer"
+        },
+        "item3": {
+            "type": "ImageCard",
+            "image": "<img src='https://zippypaws.com/app/uploads/2018/05/strawberry-waffles-1024x668.jpg'>",
+            "title": "TITLE",
+            "subtitle": "SUBTITLE",
+            "no": "$34",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam semper vulputate dolor quis vehicula. Proin lobortis nisi sed massa tempus feugiat. Sed a justo ligula. Integer"
+        },
+        "item4": {
+            "type": "QuestionCard",
+            "title": "QUESTION",
+            "subtitle": "SLOT FILLING",
+            "description": "What is your age ?"
+        },
+        "item5": {
+            "type": "QuestionCard",
+            "title": "QUESTION",
+            "subtitle": "SOME OTHER SHIT",
+            "description": "What is your blah blah ?"
+        }
+    },
+    "queries": {
+        "query1": "What is your name ?",
+        "query2": "What is your favourite color ?",
+        "query3": "I seriously need to find better examples ?",
+        "query4": "This will be my last one, from this point onwards things just repeat ?",
+        "query5": "What is your name ?",
+        "query6": "What is your favourite color ?",
+        "query7": "I seriously need to find better examples ?",
+        "query8": "This will be my last one, from this point onwards things just repeat .. Thank you folks ?"
+    }
+}
