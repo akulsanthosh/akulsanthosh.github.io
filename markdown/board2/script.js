02afc27acaffd0cf9c485dd0625211aee37ce430
Akul Santhosh <akul753@gmail.com>
2020-04-17 19:02:55 +0530

    Add files via upload

---
+++ markdown/board2/script.js
@@ -0,0 +1,37 @@
+var board = 'board2'
+
+function createSteps() {
+    string = ``
+    markdown = ``
+    Object.keys(data[board]).forEach(element => {
+        string = string + `<a href="#` + element + `">` + element + `</a><br>`
+        url = 'https://akuls.com/markdown/data/' + board + '/' + data[board][element]
+        fetch(url)
+            .then(response => response.blob())
+            .then(blob => {
+                var r = new FileReader();
+                r.onload = function (e) {
+                    var contents = e.target.result;
+                    var convertor = new showdown.Converter(),
+                        text = contents,
+                        html = convertor.makeHtml(text)
+
+                    markdown = markdown + html
+                    document.getElementById("markdown").innerHTML = markdown
+                }
+                r.readAsText(blob);
+
+            });
+    })
+    document.getElementById("step").innerHTML = string
+
+}
+
+
+function setup(data) {
+    // createBoards(data)
+    createSteps()
+    // createMarkdown("board1", data["board1"]["step1.md"])
+}
+
+setup(data)
--- markdown/script.js
+++ markdown/script.js
@@ -1,45 +1,15 @@
-var markdown = document.getElementById("markdown")
 
 function createBoards(data) {
     string = ``
+    url = 'https://akuls.com/markdown/'
     Object.keys(data).forEach(element => {
-        string = string + `<a href="javascript:void(0)" onclick="createSteps('` + element + `')"> ` + element + `</a><br>`
+        string = string + `<a href="` + url + element + `/` + `"> ` + element + `</a><br>`
     })
     document.getElementById("board").innerHTML = string
 }
 
-function createSteps(board) {
-    string = ``
-    Object.keys(data[board]).forEach(element => {
-        string = string + `<a href="javascript:void(0)" onclick="createMarkdown('` + board + `','` + data[board][element] + `')"> ` + element + `</a><br>`
-    })
-    document.getElementById("step").innerHTML = string
-    createMarkdown(board, data[board]['step1.md'])
-}
-
-function createMarkdown(board, data) {
-    url = 'https://akuls.com/markdown/data/' + board + '/' + data
-    fetch(url)
-        .then(response => response.blob())
-        .then(blob => {
-            var r = new FileReader();
-            r.onload = function (e) {
-                var contents = e.target.result;
-                var convertor = new showdown.Converter(),
-                    text = contents,
-                    html = convertor.makeHtml(text)
-
-                markdown.innerHTML = html
-            }
-            r.readAsText(blob);
-        });
-
-}
-
 function setup(data) {
     createBoards(data)
-    createSteps(data["board1"])
-    createMarkdown("board1", data["board1"]["step1.md"])
 }
 
 setup(data)
